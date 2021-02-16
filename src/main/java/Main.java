539a90233a618596f55e00d812f31b9c1ee0195c
Razercake <45364954+Razercake@users.noreply.github.com>
2021-02-16 19:14:19 +0100

    accountmapper branch created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Database branch
+        // accountmapper branch
 
         Customer c1 = new Customer("Jon");
         Account account = new Account(c1);
