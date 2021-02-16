ce1c67c08ed5bce05e18dbc051f501e865392953
Razercake <45364954+Razercake@users.noreply.github.com>
2021-02-16 19:52:35 +0100

    tadded transaction getters n setters

--- src/main/java/domain/Transaction.java
+++ src/main/java/domain/Transaction.java
@@ -1,4 +1,38 @@
 package domain;
 
 public class Transaction {
+    private int transaction_id;
+    private int acccount_id;
+    private int transaction_amount;
+
+    public Transaction(int transaction_id,int acccount_id, int transaction_amount) {
+        this.transaction_id = transaction_id;
+        this.acccount_id = acccount_id;
+        this.transaction_amount = transaction_amount;
+    }
+
+    public int getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setTransaction_id(int transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    public int getAcccount_id() {
+        return acccount_id;
+    }
+
+    public void setAcccount_id(int acccount_id) {
+        this.acccount_id = acccount_id;
+    }
+
+    public int getTransaction_amount() {
+        return transaction_amount;
+    }
+
+    public void setTransaction_amount(int transaction_amount) {
+        this.transaction_amount = transaction_amount;
+    }
+
 }
