5b0a02ecc252f1621b72a17cff23c34ce738a3cc
Razercake <45364954+Razercake@users.noreply.github.com>
2021-02-16 11:49:01 +0100

    database branch oprettet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // first commit
+        // Database branch
 
         Customer c1 = new Customer("Jon");
         Account account = new Account(c1);
