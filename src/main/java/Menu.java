8409402af540c7f8734a34bdca0e613b125cb4f1
Cjapz <Cph-ln288@cphbusiness.dk>
2021-02-16 13:06:25 +0100

    menu prototype lavet

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,26 @@
+import org.junit.jupiter.api.Test;
+
 public class Menu {
+
+    private static String menu() {
+
+        System.out.println("*********************************************");
+        System.out.println("*                                           *");
+        System.out.println("*               Velkommen til               *");
+        System.out.println("*               Ebberød Bank                *");
+        System.out.println("*                                           *");
+        System.out.println("*                                           *");
+        System.out.println("*                Konto     [1]              *");
+        System.out.println("*              Hæv penge   [2]              *");
+        System.out.println("*             Indsæt penge [3]              *");
+        System.out.println("*                Exit      [4]              *");
+        System.out.println("*                                           *");
+        System.out.println("*                                           *");
+        System.out.println("*********************************************");
+
+
+        return menu();
+    }
+
 }
+
