fd23b81293bcfec45572206354e9a1c8abf704cb
Razercake <45364954+Razercake@users.noreply.github.com>
2021-02-16 19:26:36 +0100

    added getters n setters to account class

--- src/main/java/domain/Account.java
+++ src/main/java/domain/Account.java
@@ -1,4 +1,17 @@
 package domain;
 
 public class Account {
+    private int account_id;
+
+    public Account(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
 }
