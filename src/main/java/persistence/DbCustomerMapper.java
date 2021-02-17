a9fa693d3dfdd4c4c8ced9420f964cc5b10c9c6c
Razercake <45364954+Razercake@users.noreply.github.com>
2021-02-17 12:34:05 +0100

    added delete,update,get -customer

--- src/main/java/persistence/DbCustomerMapper.java
+++ src/main/java/persistence/DbCustomerMapper.java
@@ -71,6 +71,78 @@ public class DbCustomerMapper {
             e.printStackTrace();
         }
         return customerList;
-    }}
+    }
+
+    public boolean deleteCustomer(int customer_id) {
+        boolean result = false;
+        String sql = "delete from bank.customer where customer_id = ?";
+        try (Connection connection = database.connect()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, customer_id);
+                int rowsAffected = ps.executeUpdate();
+                if (rowsAffected == 1) {
+                    result = true;
+                }
+            } catch (SQLException throwables) {
+                System.out.println("der er sket en databasefejl");
+            }
+        } catch (SQLException e) {
+            System.out.println("kære Mario, der er sket en databasefejl");
+        }
+        return result;
+    }
+
+    public boolean updateCustomer(Customer customer) {
+        boolean result = false;
+        String sql = "update bank.customer set customer_id = ?, customer_name = ?, customer_birthday = ?, customer_phone = ?, customer_address = ?, customer_saldo = ? where customeer_id = ?";
+        try (Connection connection = database.connect()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, customer.getCustomer_id());
+                ps.setString(2, customer.getCustomer_name());
+                ps.setString(3, customer.getCustomer_birthday());
+                ps.setInt(4, customer.getCustomer_phone());
+                ps.setString(5, customer.getCustomer_address());
+                ps.setInt(6, customer.getCustomer_saldo());
+                ps.setInt(7, customer.getCustomer_id());
+                int rowsAffected = ps.executeUpdate();
+                System.out.println(rowsAffected + " antal linjer ændret");
+                if (rowsAffected == 1) {
+                    result = true;
+                }
+            } catch (SQLException throwables) {
+                System.out.println("der er sket en databasefejl");
+            }
+        } catch (SQLException e) {
+            System.out.println("der er sket en databasefejl");
+        }
+        return result;
+    }
+
+    public Customer getCustomerById(int customer_id) {
+        Customer customer = null;
+        String sql = "select * from bank.customer where customer_id = ?";
+        try (Connection connection = database.connect()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, customer_id);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    String customer_name = rs.getString("customer_name");
+                    String customer_birthday = rs.getString("customer_birthday");
+                    int customer_phone = rs.getInt("customer_phone");
+                    String customer_address = rs.getString("customer_address");
+                    int customer_saldo = rs.getInt("customer_saldo");
+                    customer = new Customer(customer_id, customer_name, customer_birthday, customer_phone, customer_address, customer_saldo);
+                }
+            } catch (SQLException throwables) {
+                System.out.println("der er sket en databasefejl");
+            }
+        } catch (SQLException e) {
+            System.out.println("kære Mario, der er sket en databasefejl");
+        }
+        return customer;
+    }
 
 
+
+
+}
